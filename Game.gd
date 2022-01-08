4e14b7028b45ca69216bd5c9c6443b52160c1a02
Jonathan Lambert <jonathanlambert247@gmail.com>
2022-01-08 15:32:17 -0800

    init

---
+++ Game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var enemy = get_node("Enemy")
+onready var hud = get_node("HUD")
+onready var player = get_node("Player")
+
+func _on_EnemyWait_timeout():
+    hud.hide_ready_text()
+    enemy.set_random_direction()
+
+func _on_Coin_collected():
+    player.increase_score()
+    hud.update_score(player.score)
