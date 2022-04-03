0e05cae2f949bfda2c575b929783ec640f4ccf35
Jonathan Lambert <jonathanlambert247@gmail.com>
2022-04-03 11:09:18 -0700

    rearrange files, add signal to check user score

--- src/Game.gd
+++ src/Game.gd
@@ -5,9 +5,13 @@ onready var hud = get_node("HUD")
 onready var player = get_node("Player")
 
 func _on_EnemyWait_timeout():
-	hud.hide_ready_text()
-	enemy.set_random_direction()
+    hud.hide_ready_text()
+    enemy.set_random_direction()
 
 func _on_Coin_collected():
-	player.increase_score()
-	hud.update_score(player.score)
+    player.increase_score()
+    hud.update_score(player.score)
+
+# add enemy effect when player score reaches a multiple of 10
+func _on_Player_score_reached():
+    enemy.set_random_direction()
