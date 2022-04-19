91566f887200ac02c0224361acebb4e86a179493
jonathan <jonathanlambert247@gmail.com>
2022-04-19 14:25:36 -0700

    add play again function

--- src/menus/game_over/GameOver.gd
+++ src/menus/game_over/GameOver.gd
@@ -6,8 +6,10 @@ func _ready():
     score_label.text = str(get_node("/root/Global").score)
 
 func _on_Button_pressed():
+    play_again()
+
+func play_again():
     get_node("/root/Global").score = 0
     var error = get_tree().change_scene("res://src/Game.tscn")
     if error != OK:
         push_error(error)
-
